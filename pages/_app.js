2640a68c1a69eaf057f91a12991d2de9c8a6407a
전연지 <jeonyeonji1028@gmail.com>
2023-10-12 15:26:31 +0900

    next config

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
+import Layout from '../components/Layout'
 import NavBar from '../components/NavBar'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   )
 }
