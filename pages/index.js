7d514ac752e4f6639164e8d527f944647dd76e46
전연지 <jeonyeonji1028@gmail.com>
2023-10-12 17:18:04 +0900

    catch all, context

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,29 @@
+import Link from 'next/link'
 import Seo from '../components/Seo'
-import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 const BASE_PATH = 'https://api.themoviedb.org/3'
 
-export default function Home() {
-  const [movies, setMovies] = useState([])
-
-  useEffect(() => {
-    ;(async () => {
-      const { results } = await (await fetch(`/api/movies`)).json()
-      setMovies(results)
-    })()
-  }, [])
-
+export default function Home({ results }) {
+  const router = useRouter()
+  const onClick = (id, title) => {
+    router.push(`/movies/${title}/${id}`)
+  }
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+      {results?.map((movie) => (
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
@@ -29,6 +32,9 @@ export default function Home() {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
@@ -46,4 +52,15 @@ export default function Home() {
       `}</style>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json()
+  return {
+    props: {
+      results,
+    },
+  }
 }
